341103c96d3c50f5b1ccf7ede6d0d3f6e083ff1c
FanFan <eric93504@gmail.com>
2021-07-04 17:27:15 +0800

    specify required fields

--- model/score.go
+++ model/score.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Score struct {
 	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	JudgeID string             `json:"judgeID,omitempty" bson:"judgeID,omitempty"`
-	Row     int                `json:"row,omitempty" bson:"row,omitempty"`
-	Number  int                `json:"number,omitempty" bson:"number,omitempty"`
-	Point   float32            `json:"point,omitempty" bson:"point,omitempty"`
+	Row     int                `json:"row" bson:"row" binding:"required"`
+	Number  int                `json:"number" bson:"number" binding:"required"`
+	Point   float32            `json:"point" bson:"point" `
 }
