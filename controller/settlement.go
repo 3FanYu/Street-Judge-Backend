ebd0dd46865917790f34a3fa52856d32306d2ab8
FanFan <eric93504@gmail.com>
2021-07-04 17:28:30 +0800

    add some comments

--- controller/settlement.go
+++ controller/settlement.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
@@ -14,26 +13,28 @@ import (
 )
 
 func SettleScore(c *gin.Context) {
-	eventID := c.Query("eventID")
-	judges := getAllJudges(&eventID)
-	ch := make(chan model.JudgeScore)
+	eventID := c.Query("eventID")     // 取得 eventID
+	judges := getAllJudges(&eventID)  // 取得此 eventID 的所有judge
+	ch := make(chan model.JudgeScore) // 創建channel
 	for i := 0; i < len(*judges); i++ {
-		go getAllScores(&((*judges)[i]), ch)
+		go getAllScores(&((*judges)[i]), ch) //同步取得多位judge的所有分數，並使用channel回傳。
 	}
-	var result model.Settlement
-	for x := 0; x < len(*judges); x++ {
-		result.JudgeScore = append(result.JudgeScore, <-ch)
+	result := make([]interface{}, 0)    // 創造空slice並依序把channel收到的回傳值插入
+	for x := 0; x < len(*judges); x++ { // 有幾位judge就接收channel幾次
+		result = append(result, <-ch)
 	}
-	c.JSON(200, gin.H{
+	c.JSON(200, gin.H{ //回傳
 		"message":    true,
 		"settlement": result,
 	})
 }
 
+// 取得此 eventID 的所有judge
 func getAllJudges(eventID *string) *[]model.Judge {
 	collection := database.Db.Collection("judges")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	findOptions := options.Find().SetSort(bson.D{{"name", 1}})
+	// 查詢所有eventID符合的judge
 	cursor, err := collection.Find(ctx, bson.M{"eventID": eventID}, findOptions)
 	if err != nil {
 		log.Fatal(err)
@@ -42,10 +43,10 @@ func getAllJudges(eventID *string) *[]model.Judge {
 	if err = cursor.All(ctx, &judges); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(judges)
 	return &judges
 }
 
+// 取得該judge的所有分數
 func getAllScores(judge *model.Judge, ch chan model.JudgeScore) {
 	judgeID := judge.ID.Hex()
 	collection := database.Db.Collection("scores")
@@ -59,6 +60,7 @@ func getAllScores(judge *model.Judge, ch chan model.JudgeScore) {
 	if err = cursor.All(ctx, &scores); err != nil {
 		log.Fatal(err)
 	}
+	// 如果沒有分數就回傳空陣列
 	if len(scores) > 0 {
 		var judgeScore = model.JudgeScore{
 			ID:     judge.ID,
